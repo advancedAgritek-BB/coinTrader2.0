37b30d8859aa43c21fd376780328f7a28973a289
advancedAgritek-BB <152901032+advancedAgritek-BB@users.noreply.github.com>
2025-07-02 13:26:50 -0500

    Handle async Telegram send_message

--- crypto_bot/utils/telegram.py
+++ crypto_bot/utils/telegram.py
@@ -1,11 +1,16 @@
 from telegram import Bot
 from typing import Optional
+import inspect
+import asyncio
 
 
 def send_message(token: str, chat_id: str, text: str) -> Optional[str]:
     try:
         bot = Bot(token)
-        bot.send_message(chat_id=chat_id, text=text)
+        if inspect.iscoroutinefunction(bot.send_message):
+            asyncio.run(bot.send_message(chat_id=chat_id, text=text))
+        else:
+            bot.send_message(chat_id=chat_id, text=text)
         return None
     except Exception as e:
         return str(e)
