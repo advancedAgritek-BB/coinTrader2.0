de81a6708469766007ffd1a1a7cb6e804d99c0e5
advancedAgritek-BB <152901032+advancedAgritek-BB@users.noreply.github.com>
2025-07-05 17:49:14 -0500

    Remove duplicate config argument

--- crypto_bot/main.py
+++ crypto_bot/main.py
@@ -360,7 +360,6 @@ async def main() -> None:
             limit=100,
             use_websocket=config.get("use_websocket", False),
             force_websocket_history=config.get("force_websocket_history", False),
-            config=config,
             max_concurrent=config.get("max_concurrent_ohlcv"),
         )
 
