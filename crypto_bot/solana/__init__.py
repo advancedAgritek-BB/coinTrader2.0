e79409e8bbb7a1988037940bbee5156f8394a2f4
advancedAgritek-BB <152901032+advancedAgritek-BB@users.noreply.github.com>
2025-07-27 16:54:49 -0500

    Add Solana rug check utilities

--- crypto_bot/solana/__init__.py
+++ crypto_bot/solana/__init__.py
@@ -2,7 +2,7 @@
 
 from .watcher import NewPoolEvent, PoolWatcher
 from .meme_wave_runner import start_runner
-from .sniper_solana import score_new_pool
+from .sniper_solana import rug_check, score_new_pool
 from .runner import run
 from .api_helpers import helius_ws, fetch_jito_bundle
 from .scanner import get_solana_new_tokens
@@ -19,6 +19,7 @@ __all__ = [
     "get_solana_new_tokens",
     "start_runner",
     "score_new_pool",
+    "rug_check",
     "get_token_accounts",
     "get_pyth_price",
     "fetch_solana_prices",
