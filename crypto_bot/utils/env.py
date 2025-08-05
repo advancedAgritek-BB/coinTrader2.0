ec543b63f027278c3feb1144eecfd8bbaa83eec3
advancedAgritek-BB <152901032+advancedAgritek-BB@users.noreply.github.com>
2025-08-05 16:12:40 -0500

    fix: remove duplicate import

--- crypto_bot/utils/env.py
+++ crypto_bot/utils/env.py
@@ -1,11 +1,5 @@
 import os
-
-
-def env_or_prompt(name: str, prompt: str) -> str:
-    """Return the value of an environment variable or prompt the user."""
-    return os.getenv(name) or input(prompt)
 import sys
-
 from crypto_bot.utils.logger import LOG_DIR, setup_logger
 
 logger = setup_logger(__name__, LOG_DIR / "env.log")
