9f92e7712428bdb9ce8e37d94331967f6e53c1c0
advancedAgritek-BB <152901032+advancedAgritek-BB@users.noreply.github.com>
2025-08-06 14:05:35 -0500

    Remove redundant expected tuple in websocket test

--- tests/test_cex_executor.py
+++ tests/test_cex_executor.py
@@ -288,7 +288,6 @@ def test_get_exchange_websocket(monkeypatch):
         expected = ("key", "sec", None, "apitoken")
     else:
         expected = ("key", "sec", None, None)
-    expected = ("key", "sec", "token", "apitoken")
     assert created["args"] == expected
 
 
