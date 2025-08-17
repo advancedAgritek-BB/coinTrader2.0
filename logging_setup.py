906df29f07143225afadc55583bf693749b73c7e
advancedAgritek-BB <152901032+advancedAgritek-BB@users.noreply.github.com>
2025-08-17 10:55:53 -0500

    Disable console logging to show only monitor output

--- logging_setup.py
+++ logging_setup.py
@@ -4,6 +4,27 @@ import sys
 
 
 def setup_logging(level="INFO", logfile="bot.log", console_level=None):
+    handlers: dict[str, dict] = {
+        "file": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "level": level,
+            "formatter": "console",
+            "filename": logfile,
+            "maxBytes": 10 * 1024 * 1024,
+            "backupCount": 3,
+            "encoding": "utf8",
+        }
+    }
+    root_handlers = ["file"]
+    if console_level is not None:
+        handlers["stdout"] = {
+            "class": "logging.StreamHandler",
+            "level": console_level,
+            "formatter": "console",
+            "stream": sys.stdout,
+        }
+        root_handlers.insert(0, "stdout")
+
     LOGGING = {
         "version": 1,
         "disable_existing_loggers": False,
@@ -13,25 +34,9 @@ def setup_logging(level="INFO", logfile="bot.log", console_level=None):
                 "format": '{"t":"%(asctime)s","lv":"%(levelname)s","mod":"%(name)s","thread":"%(threadName)s","msg":"%(message)s"}'
             },
         },
-        "handlers": {
-            "stdout": {
-                "class": "logging.StreamHandler",
-                "level": console_level or level,
-                "formatter": "console",
-                "stream": sys.stdout,
-            },
-            "file": {
-                "class": "logging.handlers.RotatingFileHandler",
-                "level": level,
-                "formatter": "console",
-                "filename": logfile,
-                "maxBytes": 10 * 1024 * 1024,
-                "backupCount": 3,
-                "encoding": "utf8",
-            },
-        },
+        "handlers": handlers,
         "loggers": {
-            "": {"handlers": ["stdout", "file"], "level": level},
+            "": {"handlers": root_handlers, "level": level},
             "crypto_bot.symbols": {"level": level},
             "crypto_bot.ohlcv": {"level": level},
             "crypto_bot.trader": {"level": level},
